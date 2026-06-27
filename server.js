addbb363872b52c10a6c0ef3b5c8332e3b4f0d16
sadmansakif21-jpg <sadmansakif21@gmail.com>
2026-06-27 23:12:02 +0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('draw', (data) => {
+        socket.broadcast.emit('draw', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
